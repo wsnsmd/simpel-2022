cea8e9577b686da7eb587b2812ec5d9d2dcecbd3
wsnsmd <wsnsmd@gmail.com>
2024-10-18 08:18:14 +0800

    cek sertifikat

--- resources/views/report/dom/sertifikat/sertifikat1.blade.php
+++ resources/views/report/dom/sertifikat/sertifikat1.blade.php
@@ -182,7 +182,7 @@
         </div>
         @if(!is_null($sertifikat->spesimen))
         <div id="tt2">
-            <img src="{{ $sertifikat->spesimen }}" height="200" />
+            <img src="{{ asset($sertifikat->spesimen) }}" height="200" />
         </div>
         @endif
     </div>
